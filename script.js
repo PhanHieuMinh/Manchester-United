ac47b59e8b8fba61a9467007f02f115ab54b8788
PhanHieuMinh <phanhieuminh2212@gmail.com>
2021-05-08 12:21:01 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var info = document.querySelector('.Info');
 var main = document.querySelector('.players')
+var svd = document.querySelector('.svd')
+var info2 = document.querySelector('.Info3')
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
@@ -9,5 +11,18 @@ window.addEventListener("scroll", (event) => {
     }else{
         info.classList.remove("fixed");
         main.classList.remove("a");
+    }
+});
+
+
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    if(scroll>380){
+        info2.classList.add("fixed2");
+        svd.classList.add("a2");
+    }else{
+        info2.classList.remove("fixed2");
+        svd.classList.remove("a2");
     }
 });
